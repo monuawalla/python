dab03e2d8113fa6da54489fee0398594628f133c
Monu Awalla <monuawalla@gmail.com>
2019-02-18 19:30:02 +0530

    Update Create Line Chart using Matplotlib.py.py
    Added legend code snippet

--- Create Line Chart using Matplotlib.py.py
+++ Create Line Chart using Matplotlib.py.py
@@ -12,5 +12,8 @@ plt.title('World Population Growth', color='Blue') #Naming Title with fancy attr
 plt.yticks([0,2,4,6,8,10], ['0', '2B', '4B', '6B', '8B', '10B' ]) #Y-scale enhancement
 plt.xticks([2009, 2011, 2013, 2015, 2017, 2019]) #x-scale enhancement
 
+plt.plot(year, world_pop, label='World Population Growth', color='purple') #Creating legend / x,y-axis variables included
+plt.legend(loc = 'upper left') #displaying legend
+
 plt.show() #displays the chart
 
