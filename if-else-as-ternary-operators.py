e345493dfa2b9092a285b8deaeeaa73749ac8366
Monu Awalla <monuawalla@gmail.com>
2019-10-08 19:39:26 +0530

    Add files via upload

---
+++ if-else-as-ternary-operators.py
@@ -0,0 +1,6 @@
+#Finding greater no. using if else condition for ternary operation
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+z = x if x > y else y #using if else for ternary operation (i.e. using 3 operands)
+print("Greater number is", z)
