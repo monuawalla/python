aa94d05a7f6fcb190cb441f32368a005a5f08bbd
Monu Awalla <monuawalla@gmail.com>
2019-10-08 20:22:07 +0530

    Add files via upload

---
+++ using-while-condition-to-decrement-numbers-from-10-to-1.py
@@ -0,0 +1,5 @@
+x = 10 #initialize variable 
+while x >= 1: #while condition
+    print(x) #printing value of variable
+    x = x-1 #defining flow
+print(x) #outside while block, must print x value when while condition becomes false
